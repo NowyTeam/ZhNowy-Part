6f6783a63d00d8fb0c221b3bb878c428b2b89f6b
NowyTeam <jaj2232ajn@gmail.com>
2024-05-02 14:04:58 +0600

    Update

--- src/components/pages/Home/MainPageTB/MainPage.tsx
+++ src/components/pages/Home/MainPageTB/MainPage.tsx
@@ -1,26 +1,10 @@
 import Style from "./MainPage.module.scss";
-import Faceit from "../../../../../svg/faceit8.svg";
 
 const MainPage = () => {
   return (
     <>
-      <div className={Style.Wrapper}>
-        <div className={Style.MainBox}>
-          <div className={Style.Firts_Wrapper}>
-            <div className={Style.CS_GO_Info}>
-              <div className={Style.Logo}>
-                <img src="../../../../../public/pngwing.com (9).png" alt="" />
-              </div>
-              <div className={Style.Title}>
-                <h3>Nowy</h3>
-                <p>Nowy :3</p>
-              </div>
-              <div className={Style.FaceitLvl}>
-                <img src={Faceit} alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className={Style.wrapper}>
+        
       </div>
     </>
   );
